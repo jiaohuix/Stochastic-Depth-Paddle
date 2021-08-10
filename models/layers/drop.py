c16fa6e166fddb1220e8f65dac0a418f13e29f4f
MiuGod0126 <68314259+MiuGod0126@users.noreply.github.com>
2021-08-10 12:37:31 +0800

    Update drop.py

--- models/layers/drop.py
+++ models/layers/drop.py
@@ -1,45 +1,28 @@
 import paddle
 import paddle.nn as nn
 
-def drop_path(x, drop_prob: float = 0., training: bool = False):
+def drop_path(x,act_layer,drop_prob: float = 0., training: bool = False):
     """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
-
-    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
-    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
-    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
-    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
-    'survival rate' as the argument.
-
     """
-    if drop_prob == 0. or not training: # 如果0或者test，则直接输出激活输出  f(x),否则依概率失活
-        return x
-    keep_prob = 1 - drop_prob
-    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
-    random_tensor = keep_prob + paddle.rand(shape,dtype=x.dtype)
-    random_tensor.floor_()  # binarize
-    output = (x/keep_prob) * random_tensor #x.div_
-    return output
+    assert drop_prob>=0 and drop_prob<=1
+    if not training: #  如果是test，则直接输出激活输出  f(x),否则依概率失活
+        return act_layer(x)
+    else: # 如果有存活概率pl，则bl为p=pl的伯努利随机变量，bl=1时x被激活，反之返回x
+        keep_prob = paddle.to_tensor(1 - drop_prob) # pl 第l层的存活概率
+        bl=paddle.bernoulli(keep_prob)
+        if bl:
+            return act_layer(x)
+        else:
+            return 0
+        # return bl*act_layer(x)
 
-class Identity(nn.Layer):
-    def __init__(self):
-        super(Identity, self).__init__()
-
-    def forward(self, input):
-        return input
-
-
-class DropPath(nn.Layer):
+class StochasticLayer(nn.Layer):
     """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
     """
-    def __init__(self, drop_prob=None):
-        super(DropPath, self).__init__()
+    def __init__(self, act_layer,drop_prob=None):
+        super(StochasticLayer, self).__init__()
+        self.act_layer=act_layer
         self.drop_prob = drop_prob
 
     def forward(self, x):
-        return drop_path(x, self.drop_prob, self.training) # self.training可以获取模型的训练与否
-
-if __name__ == '__main__':
-    x=paddle.rand((3,3,1))
-    b=drop_path(x,drop_prob=0.3,training=True)
-    print(x)
-    print(b)
+        return drop_path(x,self.act_layer,self.drop_prob, self.training) # self.training可以获取模型的训练与否
